a630c5d7f57b13498feb104ca5d65e8441b89274
Tanmay1004 <149965111+Tanmay1004@users.noreply.github.com>
2025-01-03 00:40:16 +0530

    Add files via upload

---
+++ import.R
@@ -0,0 +1,10 @@
+# Install and load the necessary package
+install.packages(c("shiny", "shinythemes", "dplyr", "ggplot2", "plotly", "randomForest", "caret", "corrplot", "reshape2", "DBI", "RSQLite", "readxl","sodium"))
+
+
+
+#install.packages("readxl")  # Skip if already installed
+library(readxl)
+
+# Read the Excel file into a data frame
+wine_data <- read_excel("wine_data.xlsx")
